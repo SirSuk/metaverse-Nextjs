2a6fa4d11fb4d485b7985af99cd475786f99d4f1
Radif Elnoun <SIRIOSUKUR@GMAIL.COM>
2023-04-20 12:36:28 +0200

    first commit

---
+++ components/NewFeatures.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles";
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
+
+
+
+
+    </div>
+    New Features
+  </div>
+);
+
+export default NewFeatures;
