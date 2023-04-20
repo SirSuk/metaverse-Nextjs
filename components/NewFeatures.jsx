8f0113dafdac22f69df76e96fd343bb39d5803f0
Radif Elnoun <SIRIOSUKUR@GMAIL.COM>
2023-04-20 12:41:30 +0200

    dos

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,12 +1,9 @@
-import styles from "../styles";
+import styles from '../styles';
 
 const NewFeatures = ({ imgUrl, title, subtitle }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
-
-
-
-
+      <p>hey</p>
     </div>
     New Features
   </div>
